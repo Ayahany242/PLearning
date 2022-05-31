15d904048ab470037fd2d626c74599ff738b7190
asma245 <asmahassan372@gmail.com@gmail.com>
2022-05-31 04:06:30 -0700

    update.

---
+++ Project/SettingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingViewController.swift
+//  Project
+//
+//  Created by Asma hassan on 5/30/22.
+//  Copyright © 2022 Asma hassan. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
