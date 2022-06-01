9db8b7356766a80b1f1e9bb33acdc97b82fa7701
asma245 <asmahassan372@gmail.com@gmail.com>
2022-06-01 08:56:17 -0700

    commit.

--- Project/FSPagerViewObjcCompat.h
+++ Project/FSPagerViewObjcCompat.h
@@ -1,23 +1,23 @@
+////
+////  FSPagerViewObjcCompat.h
+////  FSPagerView
+////
+////  Created by 丁文超 on 2018/9/18.
+////  Copyright © 2018 Wenchao Ding. All rights reserved.
+////
 //
-//  FSPagerViewObjcCompat.h
-//  FSPagerView
+//#import <UIKit/UIKit.h>
+//#import <Foundation/Foundation.h>
 //
-//  Created by 丁文超 on 2018/9/18.
-//  Copyright © 2018 Wenchao Ding. All rights reserved.
+//#define FSPagerViewExtern extern
 //
-
-#import <UIKit/UIKit.h>
-#import <Foundation/Foundation.h>
-
-#define FSPagerViewExtern extern
-
-/**
- Requests that FSPagerView use the default value for a given distance.
- */
-FSPagerViewExtern NSUInteger const FSPagerViewAutomaticDistance;
-
-/**
- Requests that FSPagerView use the default value for a given size.
- */
-FSPagerViewExtern CGSize const FSPagerViewAutomaticSize;
-
+///**
+// Requests that FSPagerView use the default value for a given distance.
+// */
+//FSPagerViewExtern NSUInteger const FSPagerViewAutomaticDistance;
+//
+///**
+// Requests that FSPagerView use the default value for a given size.
+// */
+//FSPagerViewExtern CGSize const FSPagerViewAutomaticSize;
+//
--- Project/FSPagerViewObjcCompat.m
+++ Project/FSPagerViewObjcCompat.m
@@ -1,12 +1,12 @@
+////
+////  FSPagerViewObjcCompat.m
+////  FSPagerView
+////
+////  Created by Wenchao Ding on 2018/9/24.
+////  Copyright © 2018 Wenchao Ding. All rights reserved.
+////
 //
-//  FSPagerViewObjcCompat.m
-//  FSPagerView
+//#import "FSPagerViewObjcCompat.h"
 //
-//  Created by Wenchao Ding on 2018/9/24.
-//  Copyright © 2018 Wenchao Ding. All rights reserved.
-//
-
-#import "FSPagerViewObjcCompat.h"
-
-NSUInteger const FSPagerViewAutomaticDistance = 0;
-CGSize const FSPagerViewAutomaticSize = { .width = 0, .height = 0 };
+//NSUInteger const FSPagerViewAutomaticDistance = 0;
+//CGSize const FSPagerViewAutomaticSize = { .width = 0, .height = 0 };
